5513dd039122134575b89e8d063c27f3cec62942
MakerSquare Student <students@themakersquare.com>
2013-11-21 11:10:05 -0600

    Failed test for score of 20

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -19,8 +19,13 @@ describe BowlingGame do
 
 			expect(game.score).to eq(0)
 		end
-	end	
+		it 'scores the game as 20 when one pin is knocked down on each roll' do
+			game = BowlingGame.new
+			20.times { game.roll(1) }
 
+			expect(game.score).to eq(20)
+		end
+	end	
 end
 
 
