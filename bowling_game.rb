e58906cd7a616d0313c7ac0cb95d82063adcc260
MakerSquare Student <students@themakersquare.com>
2013-11-21 11:03:14 -0600

    created test for game with score = 0 and created ruby methods to pass test

--- bowling_game.rb
+++ bowling_game.rb
@@ -4,6 +4,17 @@ class BowlingGame
 	def initialize
 		@rolls = []
 	end
+
+	def roll(pins)
+	end
+
+
+	# Returns the Integer score for this game. Expects to be run after all rolls
+	# for the game have been recorded.
+	def score
+		0
+	end
+
 end
 
 
