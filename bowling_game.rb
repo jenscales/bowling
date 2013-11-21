e4b39c4e304319e8ca58c800a7b15b62173a0d5e
MakerSquare Student <students@themakersquare.com>
2013-11-21 10:32:21 -0600

    Initial files uploaded for bowling project

---
+++ bowling_game.rb
@@ -0,0 +1,19 @@
+# module Pizza
+#   class Pie
+#   	attr_accessor :toppings
+
+#   	def initialize(toppings = (Topping.new('cheese'))
+#   		@toppings = toppings
+
+#   	end
+#   end
+
+#   class Topping
+#   	attr_accessor :name, :vegetarian
+
+#   	def initialize(name, vegetarian: false)
+#   		@name = name
+#   		@vegetarian = vegetarian
+#   	end
+#   end
+# end
---
+++ spec/bowling_game_spec.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'rspec'
+require_relative '../bowling_game'
+
+# describe Pizza::Pie do
+# 	describe '.initialize' do
+# 		it 'records all of the toppings' do
+# 			toppings = [
+# 			Pizza::Topping.new('mushrooms', vegetarian: true),
+# 			Pizza::Topping.new('pepperoni')
+# 		]
+# 		pizza = Pizza::Pie.new(toppings)
+
+# 		expect(pizza.toppings).to eq(toppings)
+# 		end
+# 		it 'defaults the toppings to cheese only, if the pizza has no toppings' do
+# 				pizza = Pizza::Pie.new
+
+# 				expect(pizza.toppings.size).to eq(1)
+# 				expect(pizza.toppings.first.name).to eq('cheese')
+# 		end
+# 	end
+# end 
+
+# describe Pizza::Topping do
+# 	describe 'initialize' do
+# 		it "sets the name of the topping" do
+# 			topping = Pizza::Topping.new('olives')
+
+# 			expect(topping.name).to eq('olives')
+# 		end
+# 		it "sets whether or not the topping is vegetarian" do	
+# 			topping = Pizza::Topping.new 'bell peppers', vegetarian: true
+
+# 			expect(topping.vegetarian).to eq(true)
+# 		end
+# 	end
+# end
